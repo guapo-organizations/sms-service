322e7ceff0b1616fa739dd7b4bb6f28e23fc843b
liangyu <liangyu@22.cn>
2019-05-24 17:28:35 +0800

    配置

--- lib/config/config.go
+++ lib/config/config.go
@@ -1,11 +1,28 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
 var Email163Config *EmailConfig
 
 func init() {
 	if Email163Config == nil {
+		viper.AddConfigPath("../../config")
+		viper.SetConfigName("163email")
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Fatalln("加载不到163email的配置:", err)
+		}
+
 		Email163Config = new(EmailConfig)
+		Email163Config.Host = viper.GetString("Host")
+		Email163Config.Port = viper.GetInt("Port")
+		Email163Config.Email = viper.GetString("Email")
+		Email163Config.Passwd = viper.GetString("Passwd")
 	}
+
 }
 
 //发送email的配置
